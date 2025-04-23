2452d377396efd9aec61540ecd7f40f52be80deb
man.mok <man.mok@starkford.com>
2025-04-24 00:09:22 +0800

    fix

--- src/main/java/com/SEHS4701/group/dto/LoginRequest.java
+++ src/main/java/com/SEHS4701/group/dto/LoginRequest.java
@@ -7,6 +7,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class LoginRequest {
-    private String email;
+    private String emailAddress;
     private String password;
 }
--- src/main/java/com/SEHS4701/group/model/Item.java
+++ src/main/java/com/SEHS4701/group/model/Item.java
@@ -4,26 +4,21 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
-@Table(name = "patients")
+@Table(name = "items")
 @Getter
 @Setter
-public class Patient {
+public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String firstName;
-    private String lastName;
-    private String emailAddress;
-    private String password;
-    private String phone;
-    private LocalDateTime createdAt;
-    private LocalDateTime lastLoginAt;
-    private String gender;
-    private LocalDateTime dob;
-    @OneToMany(mappedBy = "patient")
-    private List<Appointment> appointments;
+    private String name;
+    private String image_url;
+
+    @OneToMany(mappedBy = "item")
+    private List<DentistItem> dentistItems;
+
+
 }
