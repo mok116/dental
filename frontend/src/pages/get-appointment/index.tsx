29f8fd24340984a238f87a88f9df293e4ed144be
盧柏澔 <brianlo@lubaihaodeMacBook-Air.local>
2025-04-26 14:09:07 +0800

    update: homepage redirect

--- frontend/src/pages/get-appointment/index.tsx
+++ frontend/src/pages/get-appointment/index.tsx
@@ -1,10 +1,20 @@
 import { AiOutlineSchedule } from "react-icons/ai";
-import React from "react";
+import React, { useEffect } from "react";
 import RootLayout from "@/layouts/RootLayout";
 import MultistepForm from "@/components/multistepform/MultistepForm";
+import { useRouter } from "next/router";
 
 import styles from "./GetAppointment.module.css";
 const GetAppointment: React.FC = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const jwtToken = sessionStorage.getItem('jwtToken');
+    if (!jwtToken) {
+      router.push('/login');
+    }
+  }, [router]);
+
   return (
     <RootLayout>
       <section>
